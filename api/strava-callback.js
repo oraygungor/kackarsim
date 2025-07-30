5b973becb027b3a3f2154a78f07e0842488008f4
oraygungor <99195977+oraygungor@users.noreply.github.com>
2025-07-30 10:43:13 +0200

    Update strava-callback.js

--- api/strava-callback.js
+++ api/strava-callback.js
@@ -1,1 +1,11 @@
 
+export default function handler(req, res) {
+  const clientId = process.env.STRAVA_CLIENT_ID;
+  const redirectUri = `https://kackarsim.vercel.app/api/strava-callback`; // Sitenizin adını kontrol edin
+  
+  const scope = 'read,activity:read_all,profile:read_all';
+  
+  const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
+  
+  res.redirect(302, authUrl);
+}
